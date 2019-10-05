8bb9b5800105623ef56384f8ee6c13a2b9e235b5
voidii <ruoqian@umail.ucsb.edu>
2019-10-05 16:43:37 -0700

    second change

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+    @RequestMapping("/")
     public String index() {
 	String html = "<h1>Hello World!</h1>\n" +
 		"<p>This web app is powered by \n" +
