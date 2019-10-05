f0510cf7062a8103b3c2a993f613bd0f79a6e703
voidii <ruoqian@umail.ucsb.edu>
2019-10-05 16:40:36 -0700

    first change

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
-    @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
-    }
-    
+	String html = "<h1>Hello World!</h1>\n" +
+		"<p>This web app is powered by \n" +
+		"<a href='https://github.com/voidii/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+	return html;    
+	}
 }
